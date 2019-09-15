392cea34ea71c9f28c7c2cb6bcb9ad825142a806
lourdesnrdz <lulynavarrete@hotmail.com>
2019-09-15 14:05:00 -0500

    incluir nombre de categorias

--- Lab3_Grammys/js/grammys.js
+++ Lab3_Grammys/js/grammys.js
@@ -18,6 +18,7 @@ $.ajax({
 
 
 		$('#category_types').append(newHtml)
+		loadCategoriesJSON()
 
 	},
 	error: function(errorMsg) {
@@ -26,6 +27,45 @@ $.ajax({
 
 })
 
+function loadCategoriesJSON() {
 
+	$.ajax({
+		url : 'https://lourdesnrdz.github.io/Aplicaciones-Web/Lab3_Grammys/data/grammys.json',
+		type : 'GET',
+		dataType : 'json',
+		success : function(data) {
+			$('#category_types').on('change', function(event) {
+				let id = $(this).val()
+
+				let info = data.fields
+				
+
+				for(let i = 0; i < info.length; i++) {
+					if (id == info[i].field_id) {
+						let newCategories = ''
+
+						for( let j = 0; j < info[i].categories.length; j++) {
+							newCategories += `
+								<div id='${info[i].categories[j].category_id}'>
+									<h2>${info[i].categories[j].category_name}</h2>
+								</div>
+							`
+						}
+
+						$('#nominees_section').append(newCategories)
+						console.log(newCategories)
+
+						//$('#stateCapital').val(data[i].capital)
+					}
+				}
+			})
+
+		},
+		error : function(errorMsg) {
+			console.log(errorMsg)
+		}
+
+	})
+}
 
 
