e964c959fc07f062de4286b1b4b4a0c79f1ca82a
lourdesnrdz <lulynavarrete@hotmail.com>
2019-10-08 21:15:08 -0500

    ejemplo Los Angeles

--- Lab4_weather-app/app.js
+++ Lab4_weather-app/app.js
@@ -1,7 +1,7 @@
 
 const weather = require('./weather.js')
 
-//weather.darksyForecast(-122.4233, 37.8267)
+weather.darksyForecast(-122.4233, 37.8267)
 
 weather.geocode('Monterrey', function(data) {
 	console.log(data)
