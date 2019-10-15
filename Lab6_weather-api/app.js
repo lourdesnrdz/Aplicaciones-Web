9cb87c0d5a1d9d540bb20b8a072982ee30aab337
lourdesnrdz <lulynavarrete@hotmail.com>
2019-10-14 19:27:28 -0500

    Lab 6

---
+++ Lab6_weather-api/app.js
@@ -0,0 +1,18 @@
+
+const weather = require('./weather.js')
+
+// weather.geocode('New York', function(error, data) {
+// 	if(error){
+// 		console.log(error)
+// 	} else {
+// 		weather.forecast(data.longitude, data.latitude, function(error, data) {
+// 			if (error) {
+// 				console.log(error)
+// 			} else {
+// 				console.log(data)
+// 			}
+// 		})
+// 	}
+// })
+
+
