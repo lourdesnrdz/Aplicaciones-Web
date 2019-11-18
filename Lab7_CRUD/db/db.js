c4c55aa598c090dc04c8e609762402c5f31df95f
lourdesnrdz <lulynavarrete@hotmail.com>
2019-11-18 15:51:18 -0600

    .

--- Lab7_CRUD/db/db.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose')
-const connectionURL = 'mongodb+srv://admin:admin@cluster0-ez21y.mongodb.net/lab7?retryWrites=true&w=majority'
-
-mongoose.connect(connectionURL, {
-	useNewUrlParser: true,
-	useCreateIndex:true,	//crear indexes
-	useUnifiedTopology: true
-})
